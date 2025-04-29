e14eda270dc501f87b237045e55dc15902ce366a
nakajimatoshiki <naka2jima2@gmail.com>
2025-04-29 11:01:25 +0900

    type_migration

---
+++ prisma/migrations/20250428163541_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL,
+    "email" VARCHAR NOT NULL,
+    "password" VARCHAR,
+    "createdAt" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
